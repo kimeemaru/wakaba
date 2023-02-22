03ec2fd9b98f6e62d2cc3dd36d23522503fbce0d
kimeemaru <teenmin@protonmail.com>
2023-02-22 15:13:13 -0500

    updated the thread list to be betterer

--- threadlist.php
+++ threadlist.php
@@ -58,40 +58,77 @@
    </div>
     <h1>Thread List</h1>
     <table>
-      <thead>
-        <tr>
-          <th class="thread-number">Thread</th>
-          <th>Comment</th>
-        </tr>
-      </thead>
       <tbody>
-        <!-- List all threads from /res/ folder -->
+        <!-- List all threads from /res/ folder in descending last modified order so the most recently posted in thread appears at the top. -->
         <?php
-          $dir = "res/";
-          $files = scandir($dir, SCANDIR_SORT_DESCENDING);
-          foreach ($files as $file) {
-            if (preg_match('/^(\d+)\.html$/', $file, $matches)) {
-              $thread_number = $matches[1];
-              $thread_title = "Thread $thread_number";
-              $content = file_get_contents($dir . $file);
-              if (preg_match('/<span class="filetitle">(.*?)(<a href="(.*?)">.*?<\/a>)?<\/span>/', $content, $matches)) {
-                if (isset($matches[2])) {
-                  $thread_title = $matches[2];
-                  $thread_link = $matches[3];
-                } else {
-                  $thread_link = $dir . $file;
-                }
-              } else {
-                $thread_link = $dir . $file;
-              }
-              preg_match('/<p>(.*?)<\/p>/', $content, $matches);
-              $comment = $matches[1];
-              echo "<tr>";
-              echo "<td class=\"thread-number\"><a href=\"$thread_link\">$thread_title</a></td>";
-              echo "<td class=\"comment\">$comment</td>";
-              echo "</tr>";
+        $dir = "res/";
+        $files = scandir($dir);
+        usort($files, function($a, $b) use ($dir) {
+        return filemtime($dir . $b) - filemtime($dir . $a);
+        });
+        
+        $page = isset($_GET['page']) ? max(intval($_GET['page']), 1) : 1;
+        $items_per_page = 50;
+        $offset = ($page - 1) * $items_per_page;
+        $total_items = count($files) - 2;
+        $total_pages = ceil($total_items / $items_per_page);
+        
+        if ($total_pages > 1) {
+        echo "<div style=\"text-align: center;\">";
+        if ($page > 1) {
+            echo "<a href=\"?page=" . ($page - 1) . "\">Previous</a> ";
+        }
+        for ($i = 1; $i <= $total_pages; $i++) {
+            if ($i == $page) {
+            echo "$i ";
+            } else {
+            echo "<a href=\"?page=$i\">$i</a> ";
             }
-          }
+        }
+        if ($page < $total_pages) {
+            echo "<a href=\"?page=" . ($page + 1) . "\">Next</a>";
+        }
+        echo "</div>";
+        }
+        
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>Title</th>";
+        echo "<th>Comment</th>";
+        echo "</tr>";
+        for ($i = $offset; $i < $offset + $items_per_page && $i < $total_items; $i++) {
+        $file = $files[$i + 2];
+        if (preg_match('/^(\d+)\.html$/', $file, $matches)) {
+            $thread_number = $matches[1];
+            $thread_title = "";
+            $content = file_get_contents($dir . $file);
+            if (preg_match('/<span class="filetitle">(.*?)(<a href="(.*?)">.*?<\/a>)?<\/span>/', $content, $matches)) {
+            if (isset($matches[1])) {
+                $thread_title = $matches[1];
+            }
+            if (isset($matches[2])) {
+                $thread_link = $matches[2];
+            } else {
+                $thread_link = $dir . $file;
+            }
+            } else {
+            $thread_link = $dir . $file;
+            }
+            preg_match('/<p>(.*?)<\/p>/', $content, $matches);
+            $comment = $matches[1];
+            echo "<tr>";
+            echo "<td class=\"thread-number\"><a href=\"$thread_link\">";
+            if (empty(trim($thread_title))) {
+            echo "Unnamed Thread $thread_number";
+            } else {
+            echo $thread_title;
+            }
+            echo "</a></td>";
+            echo "<td class=\"comment\">$comment</td>";
+            echo "</tr>";
+        }
+        }
+        echo "</table>";
         ?>
       </tbody>
     </table>
